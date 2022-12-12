284e50191e4ae0ff0a0dbf9dd82b9e07f0994c26
erifouD <artmandr@yandex.ru>
2022-12-13 01:59:09 +0300

    VSCommit

---
+++ Source/IntershipSwipeSt/SphereDot.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SphereDot.h"
+
+// Sets default values
+ASphereDot::ASphereDot()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASphereDot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASphereDot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
